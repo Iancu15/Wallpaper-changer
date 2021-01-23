7ee98268916601ae488cb2c421416a541134709e
Alex Iancu <56955272+Iancu15@users.noreply.github.com>
2021-01-23 22:47:03 +0200

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,14 +1,14 @@
 import os
 from PIL import Image
 
-directory = r'C:\Users\alexi\Desktop\python\Wallpapers\Day'
+directory = r'C:\Users\[USER]\Desktop\python\Wallpapers\Day'
 for filename in os.listdir(directory):
     file = directory + "\\" + filename
     image = Image.open(file)
     new_image = image.resize((1920, 1080))
     new_image.save(file)
 
-directory = r'C:\Users\alexi\Desktop\python\Wallpapers\Night'
+directory = r'C:\Users\[USER]\Desktop\python\Wallpapers\Night'
 for filename in os.listdir(directory):
     file = directory + "\\" + filename
     image = Image.open(file)
